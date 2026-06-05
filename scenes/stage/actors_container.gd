5968d79c9320255db3bdfb9c60d6dd32ccabee01
Tro0021 <abhirambellamkonda80@gmail.com>
2026-06-06 00:45:51 +0530

    Added Stage and Checkpoints

--- scenes/stage/actors_container.gd
+++ scenes/stage/actors_container.gd
@@ -8,8 +8,18 @@ const PREFAB_MAP := {
 	Collectible.Type.FOOD: preload("res://scenes/props/food.tscn")
 }
 
+const ENEMY_MAP := {
+	Character.Type.PUNK: preload("res://scenes/characters/basic_enemy.tscn"),
+	Character.Type.GOON: preload("res://scenes/characters/goon_enemy.tscn"),
+	Character.Type.THUG: preload("res://scenes/characters/thug_enemy.tscn"),
+	Character.Type.BOUNCER: preload("res://scenes/characters/igor_boss.tscn")
+}
+
+@export var player : Player
+
 func _ready() -> void:
 	EntityManager.spawn_collectible.connect(on_spawn_collectible.bind())
+	EntityManager.spawn_enemy.connect(on_spawn_enemy.bind())
 	EntityManager.spawn_shot.connect(on_spawn_shot.bind())
 
 func on_spawn_collectible(type: Collectible.Type, initial_state: Collectible.State, collectible_global_position: Vector2, collectible_direction: Vector2, initial_height: float, autodestroy: bool) -> void:
@@ -20,6 +30,12 @@ func on_spawn_collectible(type: Collectible.Type, initial_state: Collectible.Sta
 	collectible.direction = collectible_direction
 	collectible.autodestroy = autodestroy
 	call_deferred("add_child", collectible)
+
+func on_spawn_enemy(enemy_data: EnemyData) -> void:
+	var enemy : Character = ENEMY_MAP[enemy_data.type].instantiate()
+	enemy.global_position = enemy_data.global_position
+	enemy.player = player
+	add_child(enemy)
 
 func on_spawn_shot(gun_root_position: Vector2, distance_traveled: float, height: float) -> void:
 	var shot : Shot = SHOT_PREFAB.instantiate()
